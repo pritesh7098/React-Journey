9ee41fe2ed6c2f611ddfbd42941e164c154fac40
pritesh7098 <priteshdhanad2001@gmail.com>
2024-04-10 16:43:42 +0530

    04_bgChanger project completed

--- 04_bgChanger/src/App.jsx
+++ 04_bgChanger/src/App.jsx
@@ -10,7 +10,7 @@ function App() {
     >
       <div className="fixed flex flex-wrap justify-center bottom-12 inset-x-0 px-2">
         <div className="flex flex-wrap justify-center gap-3 shadow-lg bg-white px-3 py-2 rounded-3xl">
-         <button
+          <button
             onClick={() => setColor("red")}
             className="outline-none px-4 py-1 rounded-full text-white shadow-lg"
             style={{ backgroundColor: "red" }}
@@ -26,7 +26,6 @@ function App() {
             green
           </button>
 
-
           <button
             onClick={() => setColor("blue")}
             className="outline-none px-4 py-1 rounded-full text-white shadow-lg"
@@ -34,9 +33,8 @@ function App() {
           >
             blue
           </button>
-
-        </div> 
-      </div> 
+        </div>
+      </div>
     </div>
   );
 }
