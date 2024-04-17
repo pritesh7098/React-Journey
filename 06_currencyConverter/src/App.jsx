f0aa5835c10cbac94cd42dc93f5315f703b84a72
pritesh7098 <priteshdhanad2001@gmail.com>
2024-04-17 18:08:09 +0530

    Project is created and structure is set ....

---
+++ 06_currencyConverter/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+    
+    <h1 class="text-red-600 bg-cyan-400 font-bold">Heyy Pritu ❤️</h1>
+    
+    </>
+  )
+}
+
+export default App
