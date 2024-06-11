ba549fd7efa7fb9fd3dc2db78f6873da77cf3d33
hey pritesh <priteshdhanad2001@gmail.com>
2024-06-11 18:26:32 +0530

    Reducer is created for an easy access

--- 10_Redux-Toolkit/src/features/todo/todoSlice.js
+++ 10_Redux-Toolkit/src/features/todo/todoSlice.js
@@ -1,4 +1,4 @@
-// i have used redux toolkit here :
+/* // i have used redux toolkit here :
 
 import { createSlice, nanoid } from "@reduxjs/toolkit";
 // nano id generates unique id
@@ -32,3 +32,34 @@ export const todoSlice = createSlice({
 export const { addTodo, removeTodo } = todoSlice.actions;
 
 export default todoSlice.reducer;
+ */
+
+
+import {createSlice, nanoid } from '@reduxjs/toolkit';
+
+const initialState = {
+    todos: [{id: 1, text: "Hello world"}]
+}
+
+
+
+export const todoSlice = createSlice({
+    name: 'todo',
+    initialState,
+    reducers: {
+        addTodo: (state, action) => {
+            const todo = {
+                id: nanoid(), 
+                text: action.payload
+            }
+            state.todos.push(todo)
+        },
+        removeTodo: (state, action) => {
+            state.todos = state.todos.filter((todo) => todo.id !== action.payload )
+        },
+    }
+})
+
+export const {addTodo, removeTodo} = todoSlice.actions
+
+export default todoSlice.reducer
