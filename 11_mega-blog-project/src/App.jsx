455b9260e823eaee1aaea93fecd83a90255c434f
hey pritesh <priteshdhanad2001@gmail.com>
2024-06-17 18:11:21 +0530

    Started an mega-blog-project with appwrite as an backend

---
+++ 11_mega-blog-project/src/App.jsx
@@ -0,0 +1,14 @@
+import "./App.css";
+
+function App() {
+
+console.log(process.env.REACT_APP_APPWRITE_URL);
+
+  return (
+    <>
+      <h1 className=" bg-red-400 text-white">Heyy Pritu </h1>
+    </>
+  );
+}
+
+export default App;
