274372f05f1561523730eea5874107011fa2357e
hey pritesh <priteshdhanad2001@gmail.com>
2024-07-04 18:18:59 +0530

    Login page is Created ( with name change )

--- 11_mega-blog-project/src/components/Login.jsx
+++ 11_mega-blog-project/src/components/Login.jsx
@@ -46,7 +46,7 @@ function Login() {
         <p className="mt-2 text-center text-base text-black/60">
           Don&apos;t have any account?&nbsp;
           <Link
-            to="/signup"
+            to="/Signup"
             className="font-medium text-primary transition-all duration-200 hover:underline"
           >
             Sign Up
