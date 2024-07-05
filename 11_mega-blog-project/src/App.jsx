c6072fa3204770138cf3ad4d98ed5a9983fc2ab0
hey pritesh <priteshdhanad2001@gmail.com>
2024-07-05 17:54:36 +0530

    Main

--- 11_mega-blog-project/src/App.jsx
+++ 11_mega-blog-project/src/App.jsx
@@ -1,14 +1,45 @@
+import { useState, useEffect } from "react";
+import { useDispatch } from "react-redux";
 import "./App.css";
+import authService from "./appwrite/auth";
+import { login, logout } from "./store/authSlice";
+import { Outlet } from "react-router-dom";
+import { Header, Footer } from "./components";
 
 function App() {
+  // Use state hook to manage loading state
+  const [loading, setLoading] = useState(true);
 
-console.log(import.meta.env.VITE_APP_APPWRITE_URL); // because it is a vite app so it starts with import.meta and the vite prefix
+  // Use dispatch hook to dispatch actions
+  const dispatch = useDispatch();
 
-  return (
-    <>
-      <h1 className=" bg-red-400 text-white">Heyy Pritu </h1>
-    </>
-  );
+  // Use effect hook for fetching user data
+  useEffect(() => {
+    // Fetch the current user data
+    authService
+      .getCurrentUser()
+      .then((userData) => {
+        // If user data is present, dispatch action to set user data
+        if (userData) {
+          dispatch(login(userData));
+        } else {
+          dispatch(logout());
+        }
+      })
+      .finally(() => setLoading(false)); // Set loading to false after fetching user data
+  }, []);
+
+  // Render the app content
+  return !loading ? (
+    <div className="min-h-screen flex flex-wrap content-between bg-slate-300">
+      <div className="w-full block"> </div>
+      <Header />
+      <main>
+        <Outlet />
+      </main>
+      <Footer />
+    </div>
+  ) : null; // Return null when loading
 }
 
 export default App;
