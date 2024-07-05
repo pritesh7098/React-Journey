ba52fc757c4fe3f33a78f16d15fdd77637a7d12a
hey pritesh <priteshdhanad2001@gmail.com>
2024-07-05 17:55:05 +0530

    Main component is ready

--- 11_mega-blog-project/src/main.jsx
+++ 11_mega-blog-project/src/main.jsx
@@ -1,10 +1,15 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+import { Provider } from "react-redux";
+import store from "../src/store/store.js"
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <Provider store={store}> // provider to share the Redux store with all components
+      <App />
+    </Provider>
+  </React.StrictMode>
+);
