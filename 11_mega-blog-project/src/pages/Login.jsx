04d29317745bc1acce984ee67471361eeb9a4116
hey pritesh <priteshdhanad2001@gmail.com>
2024-07-06 18:31:25 +0530

    Login page is created

---
+++ 11_mega-blog-project/src/pages/Login.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Login as loginComponennt } from "./../components/Login";
+
+function Login() {
+  return (
+    <div className="py-8">
+      <loginComponennt />
+    </div>
+  );
+}
+
+export default Login;
