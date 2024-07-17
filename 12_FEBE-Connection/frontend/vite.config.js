a2b2eab2f3bacfdde8a508eb8ad7b578faf19a76
hey pritesh <priteshdhanad2001@gmail.com>
2024-07-17 10:26:15 +0530

    #

---
+++ 12_FEBE-Connection/frontend/vite.config.js
@@ -0,0 +1,12 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  server: {
+    proxy: {
+      "/api": "http://localhost:3000",
+    }, // learn more about proxy configuration here
+  },
+  plugins: [react()],
+});
